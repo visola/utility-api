93ecd442519890cf70496ddcad145d37c5d452a4
Vinicius Isola <vinicius.isola@everbridge.com>
2018-06-08 13:24:51 -0400

    Use POST method that can encode or decode

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/base64/:input', (req, res) => {
-  const input = req.params.input;
-  res.json({
-    encoded: Buffer.from(input).toString('base64'),
-    decoded: input,
-  });
+app.post('/base64', (request, response) => {
+  const { body } = request;
+  const result = {};
+  if (body.encoded) {
+    result.decoded = Buffer.from(body.encoded, 'base64').toString();
+    result.encoded = body.encoded;
+  } else if (body.decoded) {
+    result.decoded = body.decoded;
+    result.encoded = Buffer.from(body.decoded).toString('base64');
+  } else {
+    response.json({error: "No encoded nor decoded attributes provided in posted body"});
+    return;
+  }
+
+  response.json(result);
 });
 
 const port = process.env.PORT || 3000
